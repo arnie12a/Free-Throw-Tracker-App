3d4d46c3ad9de1d2a28228abefeafd0716c0e00d
Arnav Karnik <38790024+arnie12a@users.noreply.github.com>
2024-05-02 11:12:45 -0500

    signup and login component ui working

---
+++ FreeThrowTracker/config/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth"
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyB2QJcpmhitFuv7Xq4EGZ9gnc6qrf-_DQM",
+    authDomain: "freethrowtrackerapp.firebaseapp.com",
+    projectId: "freethrowtrackerapp",
+    storageBucket: "freethrowtrackerapp.appspot.com",
+    messagingSenderId: "1097631943011",
+    appId: "1:1097631943011:web:5855d998e3ce234690ab56"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth();
---
+++ FreeThrowTracker/screens/Login.js
@@ -0,0 +1,41 @@
+import React from "react";
+import { StyleSheet, Text, View, TextInput, Button } from "react-native";
+
+export default function App() {
+    const [email, onChangeEmail] = React.useState("");
+    const [password, onChangePassword] = React.useState("");
+    return (
+    <View style={styles.container}>
+        <Text>Email</Text>
+        <TextInput
+        style={styles.input}
+        onChangeText={onChangeEmail}
+        value={email}
+        ></TextInput>
+        <Text>Password</Text>
+        <TextInput
+        style={styles.input}
+        onChangeText={onChangePassword}
+        value={password}
+        secureTextEntry={true}
+        ></TextInput>
+        <Button title="Log In!" onPress={() => {}} />
+    </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "#fff",
+        alignItems: "center",
+        justifyContent: "center"
+    },
+    input: {
+        height: 40,
+        width: 200,
+        margin: 12,
+        borderWidth: 1,
+        padding: 10
+    }
+});
