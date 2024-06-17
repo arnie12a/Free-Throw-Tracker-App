ff3450b426afb26a5f853c5bd6209267efc9be6c
Arnav Karnik <38790024+arnie12a@users.noreply.github.com>
2024-06-16 23:17:42 -0500

    change before working on photography portfolio

--- ftapp/src/components/auth/register/register.jsx
+++ ftapp/src/components/auth/register/register.jsx
@@ -24,6 +24,7 @@ export default function Register() {
         e.preventDefault()
         if(!isRegistering) {
             setIsRegistering(true)
+            
             const userCredential = await doCreateUserWithEmailAndPassword(email, password)
             const user = userCredential.user;
 
