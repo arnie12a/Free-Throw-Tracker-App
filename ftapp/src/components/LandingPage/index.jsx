219de1a19ebce0e7c0df11d2f0a3b0589a51af46
Arnav Karnik <38790024+arnie12a@users.noreply.github.com>
2024-07-23 14:18:01 -0500

    update to the json

--- ftapp/src/components/LandingPage/index.jsx
+++ ftapp/src/components/LandingPage/index.jsx
@@ -8,7 +8,7 @@ export default function LandingPage() {
           <h1 className="text-4xl font-bold text-[#F7F5FB] animate-pulse">Free Throw Percentage Tracker</h1>
         </div>
         <div className="flex flex-col md:flex-row items-center justify-center">
-          <div className="md:w-1/2 text-center md:text-right md:pr-6 mb-6 md:mb-0">
+          <div className="text-center  md:pr-6 mb-6 md:mb-0">
             <img
               src="https://www.carlswebgraphics.com/basketball-graphics/free-throw-animation-2018.gif"
               alt="man shooting a free throw"
@@ -16,12 +16,7 @@ export default function LandingPage() {
               className="mx-auto md:mx-0 transform transition-transform hover:scale-110"
             />
           </div>
-          <div className="md:w-1/2 text-center md:text-left md:pl-6">
-            <h2 className="text-2xl font-semibold text-[#393d3f] mb-4">Our Mission</h2>
-            <p className="text-lg text-[#F7F5FB]">
-              Our mission is to help basketball players of all levels improve their free throw shooting. Through personalized tracking and analytics, we aim to provide insights and strategies that will help you increase your free throw percentage and become a more reliable player on the court.
-            </p>
-          </div>
+          
         </div>
       </div>
     </div>
