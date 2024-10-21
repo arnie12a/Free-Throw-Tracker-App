bf45776943a20d152da5145fa169de5bb5da1872
Arnav Karnik <38790024+arnie12a@users.noreply.github.com>
2024-10-20 23:08:25 -0500

    gettign rid of button to add stats on home page

--- ftapp/src/components/home/index.jsx
+++ ftapp/src/components/home/index.jsx
@@ -87,12 +87,7 @@ const Home = () => {
                             </h3>
 
 
-                            <a 
-                                href='/addFTSession' 
-                                className="inline-block px-6 py-2 bg-blue-600 text-white font-medium text-sm leading-tight rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out p-4"
-                            >
-                                Start tracking your free throws!
-                            </a>
+                            
                         </div>
                     </div>
                 ) : (
